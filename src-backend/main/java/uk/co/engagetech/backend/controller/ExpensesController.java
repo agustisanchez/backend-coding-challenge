f7bce29b9a642cc1932f877a9b593e9f8335180b
Agustí Sánchez <agusti.sanchez@gmail.com>
2017-06-26 00:49:28 +0200

    Redundant annotation removed | Story 1

--- src-backend/main/java/uk/co/engagetech/backend/controller/ExpensesController.java
+++ src-backend/main/java/uk/co/engagetech/backend/controller/ExpensesController.java
@@ -7,12 +7,10 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -40,9 +38,7 @@ public class ExpensesController {
 		return expenses;
 	}
 
-	// TODO Location?
 	@RequestMapping(path = "", method = RequestMethod.POST)
-	@ResponseStatus(code = HttpStatus.CREATED)
 	public ResponseEntity<?> add(@RequestBody Expense expense, UriComponentsBuilder ucb) {
 		expenses.add(expense);
 		return ResponseEntity.created(
